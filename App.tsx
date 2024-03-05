ddf2c7ca2464ff52a09318fd08c29ceb57756b1f
nipunsamaru <nipunsamaru@gmail.com>
2024-03-05 14:07:11 +0530

    authentication

--- App.tsx
+++ App.tsx
@@ -1,14 +1,107 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-const App = () => {
+import {Colors} from './src/constants/style';
+import LoginScreen from './src/screens/LoginScreen';
+import SignupScreen from './src/screens/SignupScreen';
+import TasksOverview from './src/navigator/TabBar';
+import {useContext, useEffect, useState} from 'react';
+import AuthContextProvider, {AuthContext} from './src/store/auth-context';
+import IconButton from './src/components/ui/IconButton';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import LoadingOverlay from './src/components/ui/LoadingOverlay';
+import ManageTasks from './src/screens/ManageTask';
+
+const Stack = createNativeStackNavigator();
+
+const AuthStack = () => {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {backgroundColor: Colors.primary500},
+        headerTintColor: 'white',
+        contentStyle: {backgroundColor: Colors.primary100},
+      }}
+    >
+      <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Signup" component={SignupScreen} />
+    </Stack.Navigator>
   );
 };
 
-const styles = StyleSheet.create({});
+const AuthenticatedStack = () => {
+  const authCtx = useContext(AuthContext);
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerStyle: {backgroundColor: Colors.primary500},
+        headerTintColor: 'white',
+        contentStyle: {backgroundColor: Colors.primary100},
+      }}
+    >
+      <Stack.Screen
+        name="TasksOverview"
+        component={TasksOverview}
+        options={{
+          animation: 'slide_from_bottom',
+          headerRight: ({tintColor}) => (
+            <IconButton
+              icon="log-out-outline"
+              color={tintColor}
+              size={24}
+              onPress={authCtx.logout}
+            />
+          ),
+        }}
+      />
+      <Stack.Screen name="ManageTask" component={ManageTasks} />
+    </Stack.Navigator>
+  );
+};
 
+const Navigation = () => {
+  const authCtx = useContext(AuthContext);
+  return (
+    <NavigationContainer>
+      {!authCtx.isAuthenticated && <AuthStack />}
+      {authCtx.isAuthenticated && <AuthenticatedStack />}
+    </NavigationContainer>
+  );
+};
+const Root = () => {
+  const [isTryingLogin, setIsTryingLogin] = useState(true);
+
+  const authCtx = useContext(AuthContext);
+
+  useEffect(() => {
+    async function fetchToken() {
+      const storedToken = await AsyncStorage.getItem('token');
+
+      if (storedToken) {
+        authCtx.authenticate(storedToken);
+      }
+
+      setIsTryingLogin(false);
+    }
+
+    fetchToken();
+  }, []);
+
+  if (isTryingLogin) {
+    return <LoadingOverlay message="Loading..." />;
+  }
+
+  return <Navigation />;
+};
+
+const App = () => {
+  return (
+    <>
+      <AuthContextProvider>
+        <Root />
+      </AuthContextProvider>
+    </>
+  );
+};
 export default App;
